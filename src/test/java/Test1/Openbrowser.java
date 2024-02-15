a25a9c470b090bb23cef85d8db3e912fa071b915
Sudh9003 <sudhanshusinha@gmail.com>
2024-02-15 23:35:28 +0530

    parameter Added

--- src/test/java/Test1/Openbrowser.java
+++ src/test/java/Test1/Openbrowser.java
@@ -4,16 +4,30 @@ package Test1;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Openbrowser {
-	
+	@Parameters("Browser")
 	@Test
-	public void browserLaunch() throws InterruptedException {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
+	public void browserLaunch(String browserName) throws InterruptedException {
+		WebDriver driver=null;
+		System.out.println("Browser Name is:" +browserName);
+		if(browserName.contains("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver=new ChromeDriver();
+		}else if(browserName.contains("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver=new EdgeDriver();
+		}else if(browserName.contains("firefox")){
+			WebDriverManager.firefoxdriver().setup();
+			driver=new FirefoxDriver();
+		}
+		
         driver.get("https://www.makemytrip.com/");
         driver.manage().window().maximize();
         Thread.sleep(2000);
